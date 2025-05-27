be5e6eb2c1e02ec5633aa9ab3c52678d4d9d2fc5
faer <ferikson77@gmail.com>
2025-05-27 22:46:18 +0200

    feat: user inputs number of colors generated

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,16 +1,19 @@
-module Lib(
+module Lib
+(
  mainfunc
  ) where
 import System.Random.Stateful
 import Control.Monad (replicateM)
 import Control.Foldl as Foldl 
 
--- a component of RGB colors is an Int
+-- a component of RGB colors is a Double 0-1
 type Component = Double
 
+-- defining RGB color type
 data RGB = RGB Component Component Component
  deriving (Eq, Ord)
 
+-- Implementing show instance to print RGB on more common 0-255 scale
 instance Show RGB where
  show (RGB r g b) = let display = show . round . (*255) in
                    "RGB " ++ display r ++ " " ++ display g ++ " " ++ display b 
@@ -18,6 +21,7 @@ instance Show RGB where
 integerToDouble :: Integer -> Double
 integerToDouble = fromInteger
 
+-- Implementing RGB as an instance of Num
 instance Num RGB where
  (+) (RGB r1 g1 b1) (RGB r2 g2 b2) = RGB (r1+r2) (g1+g2) (b1+b2)
  (-) (RGB r1 g1 b1) (RGB r2 g2 b2) = RGB (r1-r2) (g1-g2) (b1-b2)
@@ -25,6 +29,7 @@ instance Num RGB where
  abs (RGB r1 g1 b1) = RGB (abs r1) (abs g1) (abs b1)
  fromInteger i = RGB x x x where x = integerToDouble i 
 
+-- Implementing RGB as an instance of Fractional 
 instance Fractional RGB where
  (/) (RGB r1 g1 b1) (RGB r2 g2 b2) = RGB (r1/r2) (g1/g2) (b1/b2)
  fromRational r = RGB f f f where f = fromRational r
@@ -37,14 +42,6 @@ instance Monoid RGB where
 
 instance Uniform RGB where
  uniformM g = RGB <$> uniformRM (0,1) g <*> uniformRM (0,1) g <*> uniformRM (0,1) g
-
--- Definition of the three primary colors
-red :: RGB
-red   = RGB 1 0 0
-green :: RGB
-green = RGB 0 1 0
-blue :: RGB
-blue  = RGB 0 0 1
 
 -- Euclidean distance
 euclideanDistance :: RGB -> RGB -> Double
@@ -96,10 +93,16 @@ samplenRGB n = replicateM n . sampleRGB
 
 mainfunc :: IO ()
 mainfunc = do
-            putStrLn "Provide an integer seed"
+            putStrLn "Provide an integer seed: "
             inp <- getLine
             let seed = read inp
-            let randomColors = runStateGen_ (mkStdGen seed) (samplenRGB 1000)
-            let out = kMeans [red, green, blue] randomColors
+            let randomGenerator = mkStdGen seed
+            putStrLn "How many colors would you like?: "
+            inp2 <- getLine
+            let user_k = read inp2
+            let randomColors = runStateGen_ randomGenerator (samplenRGB 1000)
+            let initKmeans = runStateGen_ randomGenerator (samplenRGB user_k) 
+            let out = kMeans initKmeans randomColors
             let newMeans = Prelude.map fst out
+            putStrLn "generated palette: "
             print newMeans
